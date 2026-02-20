4a37fd3dbd4b9efe3a351b4d2b6a6b7bf4624246
Landon <landoncbrown22@gmail.com>
2026-02-20 17:03:12 -0500

    Implement searchInsert method in Solutions class
    Added a searchInsert method to find the target index.

--- main.py
+++ main.py
@@ -44,3 +44,21 @@ class Solutions:
                 n += d[s[i]]
                 
         return n
+
+    # search insert
+    def searchInsert(self, nums: List[int], target: int) -> int:
+            low = 0
+            high = len(nums) - 1
+    
+            while low <= high:
+                mid = low + (high - low) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+    
+            return low
+
+
