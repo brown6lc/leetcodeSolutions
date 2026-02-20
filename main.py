2fc38db1ae6538a1e944375d599b01a3c330cd63
Landon <landoncbrown22@gmail.com>
2026-02-20 17:01:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+class Solutions:
+
+    # palindrome checker
+    def isPalindrome(self, x: int) -> bool:
+        if str(x)[::-1] == str(x):
+            return True
+        return False
+    
+    # numerical solution
+    def isPalindrome(self, x: int) -> bool:
+        # negative numbers and numbers ending in zero
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reversed_half = 0
+
+        # reverse half of the number
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+        
+        # for even digits: x == reversed_half
+        # for odd digits: x == reversed_half // 10
+        return x == reversed_half or x == reversed_half // 10
+
+    # roman numeral to int
+    def romanToInt(self, s: str) -> int:
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        n = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and d[s[i]] < d[s[i + 1]]:
+                n -= d[s[i]]
+            else:
+                n += d[s[i]]
+                
+        return n
